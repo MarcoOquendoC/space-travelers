29ee1fdf23e37d552b32e11ba79498eb82a73be2
Marco Oquendo <marcooquendoc@gmail.com>
2022-10-04 12:17:12 -0500

    Correct linter errors

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import logo from './assets/logo.png'
+import { NavLink } from 'react-router-dom';
+import logo from './assets/logo.png';
 
 const Navigation = () => {
+  const hello = "Space Travelers' Hub";
   return (
     <div>
-    <header>
-      <div className="logoContainer">
-        <img className="logo" src={logo} alt="logo" />
-       <p className='logoP'>Space Travelers' Hub</p>
-       </div>
-      <ul className='links'>
-        <li className="link"><Link to="/" >Display</Link></li>
-        <li className="link"><Link to="/Missions" >Missions</Link></li>
-        <li className="link"><Link to="/Dragons" >Dragons</Link></li>
-        <li className="link">|</li>
-        <li className="link"><Link to="/Myprofile" >MyProfile</Link></li>
-      </ul>
-    </header>  
-    <hr />
+      <header>
+        <div className="logoContainer">
+          <img className="logo" src={logo} alt="logo" />
+          <p className="logoP">
+            {hello}
+          </p>
+        </div>
+        <ul className="links">
+          <li className="link"><NavLink to="/">Display</NavLink></li>
+          <li className="link"><NavLink to="/Missions">Missions</NavLink></li>
+          <li className="link"><NavLink to="/Dragons">Dragons</NavLink></li>
+          <li className="link">|</li>
+          <li className="link"><NavLink to="/Myprofile">MyProfile</NavLink></li>
+        </ul>
+      </header>
+      <hr />
     </div>
   );
 };
