ce59b14e9dc9b85cfdbd78eac00a7cc2f59d5d65
Marco Oquendo <marcooquendoc@gmail.com>
2022-10-06 01:59:42 -0500

    Configure store only for missions

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,26 +1,10 @@
-import {
-  applyMiddleware,
-  combineReducers,
-  configureStore,
-} from '@reduxjs/toolkit';
-import thunk from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import missionsReducer from './missions/missions';
-import rocketsReducer from './rockets/rockets';
-import dragonsReducer from './dragons/dragons';
 
-// root Reducer
-const rootReducer = combineReducers({
-  missions: missionsReducer,
-  rockets: rocketsReducer,
-  dragons: dragonsReducer,
-});
-
-// Redux store
-const store = configureStore(
-  {
-    reducer: rootReducer,
+const store = configureStore({
+  reducer: {
+    missions: missionsReducer,
   },
-  applyMiddleware(thunk),
-);
+});
 
 export default store;
