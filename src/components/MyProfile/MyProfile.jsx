dd3f7c5f64e4e0d559d235370860473d0eeafe0b
ademola <ademolaowoeye07@gmail.com>
2022-10-06 21:19:53 +0100

    Displayed reservations on the profile page

--- src/components/MyProfile/MyProfile.jsx
+++ src/components/MyProfile/MyProfile.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <div className="profiles">
-    <div>
-      <h2>My Rockets</h2>
-    </div>
-    <div>
-      <h2>My Dragons</h2>
-    </div>
-    <div>
-      <h2>My Missions</h2>
-    </div>
-  </div>
-);
+function Profile() {
 
-export default { MyProfile };
+  // Dragons
+  const dragons = useSelector((state) => state.dragons);
+  const reservedDragons = dragons.filter((dragon) => dragon.dragonReserved);
+
+  const listReservedDragons = reservedDragons.map((reservedDragon) => (
+    <tbody key={reservedDragon.dragonId}>
+      <tr>
+        <td>
+          {reservedDragon.name}
+        </td>
+      </tr>
+    </tbody>
+  ));
+
+  return (
+    <div className="profiles">
+      <div>
+        <h2>My Dragons</h2>
+        <table className="table table-bordered">{listReservedDragons}</table>
+      </div>
+    </div>
+  );
+}
+
+export default Profile;
