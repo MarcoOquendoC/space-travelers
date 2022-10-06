6497602e5aa4f00f9eb4c0796caf54e8168ca5ce
Marco Oquendo <marcooquendoc@gmail.com>
2022-10-06 02:03:29 -0500

    Update Missions component

--- src/components/missions/Missions.jsx
+++ src/components/missions/Missions.jsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import Navigation from '../Navbar';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import MissionsTable from './MissionsTable';
+import { fetchingMissionsInfo } from '../../redux/missions/missions';
 
-function Missions() {
+let saveJoin = false;
+
+const Missions = () => {
+  const missions = useSelector((state) => state.missions);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (saveJoin === false) {
+      saveJoin = true;
+      dispatch(fetchingMissionsInfo());
+    }
+  }, [dispatch]);
+
   return (
-    <div>
-      <Navigation />
-      kindly Display the mission here my friend yeahhh!
-    </div>
+    <table>
+      <thead>
+        <tr className="missionRow">
+          <th className="missionName">Mission</th>
+          <th className="missionInfo">Description</th>
+          <th className="missionStatus">Status</th>
+          <th className="missionToggle">Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <MissionsTable missions={missions} />
+      </tbody>
+    </table>
   );
-}
+};
 
 export default Missions;
