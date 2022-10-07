09deb4e572214747c1fa0902032e4e0f1fb53566
Marco Oquendo <marcooquendoc@gmail.com>
2022-10-07 12:10:17 -0500

    Erase empty line to pass linters

--- src/components/MyProfile/MyProfile.jsx
+++ src/components/MyProfile/MyProfile.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 function Profile() {
-
   // Dragons
   const dragons = useSelector((state) => state.dragons);
   const reservedDragons = dragons.filter((dragon) => dragon.dragonReserved);
